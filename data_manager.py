8321165d7abbd365786e2521e3891aba7620adf6
Riyansyah <mriyansyah112@gmail.com>
2026-03-08 15:21:31 +0700

    feat: implement OS-based data path and high-res app icons

--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,18 @@
-# data_manager.py
 import json
 import os
+import platform
 
-DATA_FILE = "albion_truth_data.json"
+def get_db_path():
+    if platform.system() == "Windows":
+        base_dir = os.path.join(os.environ.get('APPDATA'), "AlbionCraftingMaster")
+    else:
+        base_dir = os.path.join(os.path.expanduser("~"), ".config", "AlbionCraftingMaster")
+
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir) 
+    return os.path.join(base_dir, "albion_truth_data.json")
+
+DATA_FILE = get_db_path()
 
 def save_to_json(data):
     with open(DATA_FILE, "w") as f: 
